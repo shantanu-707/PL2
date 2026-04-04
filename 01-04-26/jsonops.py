3ed1006f59214eeac4663aeb4a8f6da8ac3485b0
shant <shantanu0116@gmail.com>
2026-04-04 16:57:50 +0530

    Operations on JSON file and binary data type.

--- 01-04-26/jsonops.py
+++ 01-04-26/jsonops.py
@@ -1,38 +1,48 @@
 import json
 import csv
 
-# def writetojson(dict):
-#     with open("data.json",'w') as jf:
-#         json.dump(dict,jf)
-#
-# def readjson(file):
-#     with open(file,'r') as jf:
-#         data = json.load(jf)
-#         return data
-#
-# dict = {}
-# while True:
-#     intro = input("Enter new data? Y/N : ")
-#     if intro == "Y":
-#         name = input("Enter your name : ")
-#         age = int(input("Enter your age : "))
-#         salary = int(input("Enter your salary : "))
-#         data = [{"name": name, "age": age, "salary": salary}]
-#
-#     elif intro == "N":
-#         break
-# writetojson(dict)
-#
-# jsondata = readjson("data.json")
+def writetojson(dict):
+    with open("data.json",'w') as jf:
+        json.dump(dict,jf)
 
-with open("data.json") as jf:
-    e = json.load(jf)
+def readjson(file):
+    with open(file,'r') as jf:
+        data = json.load(jf)
+        return data
 
-data = e
+def jsontocsv(jsondatalocal,file):
+    with open(file, 'w') as cf:
+        cw = csv.writer(cf)
+        cw.writerow(jsondatalocal)
 
-with open("jsontocsv.csv", 'w') as cf:
-    cw = csv.writer(cf)
-    cw.writerow(data)
+emp = []
+while True:
+    intro = input("Enter new data? Y/N : ")
+    if intro == "Y":
+        name = input("Enter your name : ")
+        age = int(input("Enter your age : "))
+        salary = int(input("Enter your salary : "))
+        data = {"name":name,"age":age,"salary":salary}
+        emp.append(data)
+
+    elif intro == "N":
+        break
+file = input("Enter file name : ") + ".csv"
+writetojson(emp)
+
+jsondata = readjson("data.json")
+
+jsontocsv(jsondata,file)
+
+""""""
+# with open("data.json") as jf:
+#     e = json.load(jf)
+#
+# data = e
+#
+# with open("jsontocsv.csv", 'w') as cf:
+#     cw = csv.writer(cf)
+#     cw.writerow(data)
 
 
 
